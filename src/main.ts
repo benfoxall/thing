72936fe40d8933511c03abdfc62568b04d63773d
Ben Foxall <benfoxall@gmail.com>
2023-01-01 17:50:04 +0000

    Print session qr code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 import "./style.css";
+import QRious from "qrious";
+
+const sessionID = crypto.randomUUID();
+
+console.log(sessionID);
+
+const { origin, pathname } = window.location;
+const base = `${origin}${pathname}${new URLSearchParams({
+  session: sessionID,
+})}`;
+
+var qr = new QRious({
+  value: base,
+});
+
+const u = qr.toDataURL();
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>Thing</h1>
+    <img src=${u} />
   </div>
 `;
